c84b0cda0f40e6d9eedd7a189e6eabcfce3e7dbe
TGreenReaper <140297612+TGreenReaper@users.noreply.github.com>
2023-11-21 09:27:33 -0600

    Update main.py

--- source/main.py
+++ source/main.py
@@ -3,7 +3,8 @@ from colorama import Fore
 import random
 
 save = True
-
+filename = True
+file_name = None
 
 print("--------- IR_Remote File Gen by GREEN REAPER ---------" + Fore.BLUE)
 remote_path = input("REMOTE PATH: ")
@@ -18,8 +19,12 @@ downh = input("Down button when Held: ")
 lefth = input("Left button when Held: ")
 righth = input("Right button when Held: ")
 okh = input("Ok button when held: ")
+if(filename == True):
+   file_name = input("File Name: " + Fore.RED)
+   
 
 outp = "REMOTE: " + remote_path + "\n" + "UP: " + up + "\n" + "DOWN: " + down + "\n" + "LEFT: " + left + "\n" + "RIGHT: " + right + "\n"  + "OK: " + ok + "\n" + "BACK: " + back + "\n" + "UPHOLD: " + uph + "\n" + "DOWNHOLD: " + downh + "\n" + "LEFTHOLD: " + lefth + "\n"  + "RIGHT: " + righth + "\n"  + "OKHOLD: " + okh + "\n" 
+
 
 
 def file():
@@ -40,12 +45,20 @@ def file():
     print("OKHOLD:" + okh)
     print("\n\n\n")
     print(Fore.CYAN)
+    if(filename == True):
+        
+        with open("outputs/" + str(file_name), "w") as file:
+         file.write(outp)
+        print("Saved " + file_name + ".txt to outputs successfully!"   + Fore.GREEN)
+
+    else:
+       with open("outputs/output.txt" , "w") as file:
+         file.write(outp)
     print("---------Output has been saved to outputs/output.txt---------")
 
 file()
 
-with open("outputs/output.txt" , "w") as file:
-    file.write(outp)
+
 
 
 

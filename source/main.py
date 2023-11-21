da151f86304597db68e7716b664ec2733030ae32
TGreenReaper <140297612+TGreenReaper@users.noreply.github.com>
2023-11-21 08:30:50 -0600

    Source Code

---
+++ source/main.py
@@ -0,0 +1,57 @@
+import colorama
+from colorama import Fore
+import random
+
+save = True
+
+
+print("--------- IR_Remote File Gen by GREEN REAPER ---------" + Fore.BLUE)
+remote_path = input("REMOTE PATH: ")
+up = input("Up button:")
+down = input("Down button: ")
+left = input("Left Button: ")
+right = input("Right button: ")
+ok = input("Ok button: ")
+back = input("Back button: ")
+uph = input("Up button when Held: ")
+downh = input("Down button when Held: ")
+lefth = input("Left button when Held: ")
+righth = input("Right button when Held: ")
+okh = input("Ok button when held: ")
+
+outp = "REMOTE: " + remote_path + "\n" + "UP: " + up + "\n" + "DOWN: " + down + "\n" + "LEFT: " + left + "\n" + "RIGHT: " + right + "\n"  + "OK: " + ok + "\n" + "BACK: " + back + "\n" + "UPHOLD: " + uph + "\n" + "DOWNHOLD: " + downh + "\n" + "LEFTHOLD: " + lefth + "\n"  + "RIGHT: " + righth + "\n"  + "OKHOLD: " + okh + "\n" 
+
+
+def file():
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    print("--------- Your Complete Infrared Remote File ---------" + Fore.GREEN)
+    print("\n")
+    print("REMOTE: " + remote_path)
+    print("UP: " + up)
+    print("DOWN: " + down)
+    print("LEFT: " + left)
+    print("RIGHT: " + right)
+    print("OK: " + ok)
+    print("BACK: " + back)
+    print("UPHOLD:" + uph)
+    print("DOWNHOLD:" + downh)
+    print("LEFTHOLD:" + lefth)
+    print("RIGHTHOLD:" + righth)
+    print("OKHOLD:" + okh)
+    print("\n\n\n")
+    print(Fore.CYAN)
+    print("---------Output has been saved to outputs/output.txt---------")
+
+file()
+
+with open("outputs/output.txt" , "w") as file:
+    file.write(outp)
+
+
+
+input("EXIT:")
+
+
+
+
+
---
+++ source/readme.md.txt
@@ -0,0 +1,1 @@
+<h1> You may not copy without permissiona and claim its yours, thats skidding</h1>
